d22f4114b005080f5639dd97dcf1fe2e694952b7
suryateja90 <suryafacts@gmail.com>
2022-01-20 16:16:13 +0530

    home page ver1

--- eats/src/app/app-routing.module.ts
+++ eats/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'home',
+    loadChildren: () => import('./features/features.module').then(m => m.FeaturesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
